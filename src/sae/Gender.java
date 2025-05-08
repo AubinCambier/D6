40b0d9c519fba1f521ae360b2faa3508576e7190
Mathéo Lelong <matheo778@gmail.com>
2025-05-08 19:47:49 +0200

    Gender

--- src/sae/Gender.java
+++ src/sae/Gender.java
@@ -1,5 +1,15 @@
 package sae;
 
 public enum Gender {
-    FEMALE,MALE,OTHER;
+    FEMALE("female"),MALE("male"),OTHER("other");
+
+    private String genre;
+
+    private Gender(genre){
+        genre = genre;
+    }
+
+    public String getGenre(){
+        return this.genre;
+    }
 }
