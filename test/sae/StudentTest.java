55c1d1c555056b64f5acaa9e0dcccccc77f51ff5
Matthieu <calmatthieu@yahoo.com>
2025-05-12 15:38:50 +0200

    testStudents

--- test/sae/StudentTest.java
+++ test/sae/StudentTest.java
@@ -1,3 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sae.Student;
+
+import java.time.LocalDate;
+
 public class StudentTest {
-    
+
+    Student student1, student2, student3, student4, student5;
+
+    student1=new Student("Matthieu","A","02-05-2006","France","M","M","no","no","no","no","lalala"),student2=new Student("Mathéo","Lelong","01-01-2007","France","M","F","no","no","no","no","histoire"),student3=new Student("Lucie","Martin","15-08-2005","France","F","M","yes","no","végétarien","aucune","danse"),student4=new Student("Carlos","Gomez","23-03-2006","Espagne","M","F","no","yes","aucune","halal","musique"),student5=new Student("Aya","Tanaka","12-12-2007","Japon","F","M","yes","yes","sans gluten","végétarien","dessin");
 }
