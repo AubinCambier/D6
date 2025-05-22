7916a46fc12149091cbe14974346826a645baa03
Matthieu <calmatthieu@yahoo.com>
2025-05-22 10:12:22 +0200

    import csv

--- src/sae/ListAffectation.java
+++ src/sae/ListAffectation.java
@@ -1,14 +1,16 @@
 package sae;
 
 import java.util.ArrayList;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ListAffectation {
     /**liste d'etudiants */
     private ArrayList<Student> students;
     /**liste d'affectation */
     private ArrayList<Affectation> affectations;
-    private final static String RESSOURCES_PATH = Paths.get("","");
 
     /**constructeur principale initialisant la liste d'étudiants et la liste d'affectation */
     public ListAffectation() {
@@ -29,12 +31,35 @@ public class ListAffectation {
         this.students.add(student);
     }
 
-    public void chargerCSV() {
-        // ...
+    public static void chargerCSV() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(System.getProperty("user.dir")).append(File.separator).append("doc").append(File.separator).append("infoetu.csv");
+
+        System.out.println(""+sb.toString());
+
+        File csvFile = new File(sb.toString());
+
+         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                String[] colonnes = ligne.split(";"); 
+                for (String valeur : colonnes) {
+                    System.out.print(valeur.trim() + " | ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**methode permettant de verifier si le critère est valide */
     public void verifierValiditeCritere() {
         // ...
+    }
+    public static void main(String[] args) {
+        chargerCSV();
     }
 }
