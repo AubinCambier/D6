ccb0e1306034c6b8e3e5f45de8b07cbf79aebe43
Aubin <aubincambier.etu@univ-lille.fr>
2025-06-06 17:42:26 +0200

    ajout commentaire javadoc sae

--- src/sae/Affectation.java
+++ src/sae/Affectation.java
@@ -1,22 +1,32 @@
 package sae;
 
 /**
- * Cette classe représente une affectation entre un hôte et un visiteur.
- * Elle stocke aussi le score d'affinité entre eux.
+ * Représente une affectation entre un hôte et un visiteur.
+ * Cette classe stocke les informations des deux étudiants ainsi que leur score d'affinité.
+ * 
+ * @author <a href="mailto:aubin2109@gmail.com">Aubin B</a>
+ * @author <a href="mailto:matheo778@gmail.com">Mathéo D</a>
+ * @author <a href="mailto:calmatthieu@yahoo.com">Mathieu C</a>
+ * @author <a href="mailto:aymericjacquey@gmail.com">Aymeric C</a>
+ * @version 21.0.7
  */
 public class Affectation {
-    /** l'étudiant qui reçoit */
+    
+    /** L'étudiant qui reçoit le visiteur. */
     private Student hote;
 
-    /** l'étudiant visiteur */
+    /** L'étudiant visiteur. */
     private Student visiteur;
 
-    /** la valeur d'affinité entre les deux */
+    /** La valeur d'affinité entre l'hôte et le visiteur. */
     private double affinite;
 
     /**
-     * Constructeur qui initialise l'hôte, le visiteur,
-     * et calcule leur affinité.
+     * Constructeur de l'affectation.
+     * Initialise l'hôte et le visiteur, puis calcule leur affinité.
+     *
+     * @param visiteur L'étudiant visiteur.
+     * @param hote L'étudiant qui accueille le visiteur.
      */
     public Affectation(Student visiteur, Student hote) {
         this.hote = hote;
@@ -24,27 +34,50 @@ public class Affectation {
         this.affinite = visiteur.calculAffinite(hote);
     }
 
-    /** Renvoie l'étudiant hôte.*/
+    /**
+     * Renvoie l'étudiant hôte.
+     *
+     * @return L'étudiant qui reçoit le visiteur.
+     */
     public Student getHote() {
         return hote;
     }
 
-    /**Renvoie l'étudiant visiteur.*/
+    /**
+     * Renvoie l'étudiant visiteur.
+     *
+     * @return L'étudiant qui est affecté à un hôte.
+     */
     public Student getVisiteur() {
         return visiteur;
     }
 
-    /**Renvoie la valeur d'affinité.*/
+    /**
+     * Renvoie la valeur d'affinité entre l'hôte et le visiteur.
+     *
+     * @return La valeur de l'affinité calculée.
+     */
     public double getAffinite() {
         return affinite;
     }
 
-    /**Modifie la valeur d'affinité.*/
-    public void setAffinite(int affinite) {
+    /**
+     * Modifie la valeur d'affinité entre l'hôte et le visiteur.
+     *
+     * @param affinite La nouvelle valeur d'affinité à définir.
+     */
+    public void setAffinite(double affinite) {
         this.affinite = affinite;
     }
-    public String toString(){
-        return "["+visiteur.getNom() + ", "+visiteur.getPrenom()+"] "+ this.affinite+" ["+hote.getNom()+", "+hote.getPrenom()+"]";
+
+    /**
+     * Retourne une représentation textuelle de l'affectation.
+     * Affiche le visiteur, la valeur d'affinité et l'hôte.
+     *
+     * @return Une chaîne de caractères représentant l'affectation.
+     */
+    @Override
+    public String toString() {
+        return "[" + visiteur.getNom() + ", " + visiteur.getPrenom() + "] " + this.affinite + " [" + hote.getNom() + ", " + hote.getPrenom() + "]";
     }
 }
-
--- src/sae/Student.java
+++ src/sae/Student.java
@@ -1,27 +1,40 @@
 package sae;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.lang.Math;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Classe représentant un étudiant avec ses informations personnelles et ses préférences.
+ * Permet de stocker et manipuler les données d'un étudiant pour un système de jumelage.
+ * 
+ * @author <a href="mailto:aubin2109@gmail.com">Aubin B</a>
+ * @author <a href="mailto:matheo778@gmail.com">Mathéo D</a>
+ * @author <a href="mailto:calmatthieu@yahoo.com">Mathieu C</a>
+ * @author <a href="mailto:aymericjacquey@gmail.com">Aymeric C</a>
+ * @version 21.0.7
+ */
 
 public class Student {
-    /**atribu permettant de donné d'associé les information d'un etudiants, c'est cette hashMap qui est initialisé par le constructeur */
+    /**
+     * Map contenant les informations de l'étudiant.
+     * Les clés correspondent aux critères définis dans la map statique 'critere'.
+     */
     private Map<String, String> information;
 
-    // on instancie tous les criètès d'un étudiant
-
-    /**liste static permettant de donner le type de chacune des clée */
+    /**
+     * Map statique définissant les critères d'un étudiant et leur type.
+     * T = Texte, D = Date (format "AAAA-MM-JJ"), B = Booléen ("true"/"false")
+     */
     public static Map<String, String> critere = new HashMap<String, String>();
 
-    /**initialisation du tableau des information du clients avec du coté clé les critère et du coté valeurs, le type */
+    // Initialisation des critères et de leurs types
     static {
         critere.put("prenom", "T");
         critere.put("nom", "T");
-        critere.put("dateNaissance", "D");      // format "AAAA,MM,JJ"
+        critere.put("dateNaissance", "D");      // format "AAAA-MM-JJ"
         critere.put("pays", "T");
         critere.put("gender", "B");             // true = garçon 
         critere.put("pairGender", "B");         // true = garçon
@@ -33,16 +46,33 @@ public class Student {
         critere.put("hobbies", "T");
     }
 
-    /**  constructeur du HashMap (s'il n'existe pas)*/
+    /**
+     * Constructeur par défaut initialisant la map d'informations.
+     */
     public Student() {
         information = new HashMap<>();
     }
 
-    /**  constructeur ajoutant un Student dans la liste HashMap d'étudiants */
+    /**
+     * Constructeur complet initialisant toutes les informations de l'étudiant.
+     * Effectue des validations sur les types de données.
+     * 
+     * @param prenom Prénom de l'étudiant
+     * @param nom Nom de l'étudiant
+     * @param dateNaissance Date de naissance au format "AAAA-MM-JJ"
+     * @param pays Pays d'origine
+     * @param gender Genre
+     * @param pairGender Genre souhaité pour le partenaire
+     * @param guestAnimalAllergy Allergie aux animaux de l'invité
+     * @param hostHasAnimal Présence d'animaux chez l'hôte
+     * @param guestFood Préférences alimentaires de l'invité (séparées par des virgules)
+     * @param hostFood Préférences alimentaires de l'hôte (séparées par des virgules)
+     * @param history Historique de l'étudiant
+     * @param hobbies Centres d'intérêt (séparés par des virgules)
+     */
     public Student(String prenom, String nom, String dateNaissance, String pays, String gender, String pairGender,
             String guestAnimalAllergy, String hostHasAnimal, String guestFood, String hostFood, String history,
             String hobbies) {
-        // appel le constructeur student();
         this();
         try{
             if(!(prenom instanceof String)){
@@ -62,7 +92,6 @@ public class Student {
             System.out.println("WrongInformationException : date invalide");
         }
         try{
-
             if(!(pays instanceof String)){
                 throw new WrongInformationException("pays invalide");
             }
@@ -136,124 +165,194 @@ public class Student {
         }
     }
 
-
-
-    /** Définit le prénom de l'étudiant. */
+    /**
+     * Définit le prénom de l'étudiant.
+     * @param prenom Le prénom à enregistrer
+     */
     public void setPrenom(String prenom) {
         information.put("prenom", prenom);
     }
 
-    /** Définit le nom de l'étudiant. */
+    /**
+     * Définit le nom de l'étudiant.
+     * @param nom Le nom à enregistrer
+     */
     public void setNom(String nom) {
         information.put("nom", nom);
     }
 
-    /** Définit la date de naissance de l'étudiant au format ISO (yyyy-MM-dd). */
+    /**
+     * Définit la date de naissance de l'étudiant.
+     * @param dateNaissance La date au format "AAAA-MM-JJ"
+     */
     public void setDateNaissance(String dateNaissance) {
         information.put("dateNaissance", dateNaissance);
     }
 
-    /** Définit le pays d'origine de l'étudiant. */
+    /**
+     * Définit le pays d'origine de l'étudiant.
+     * @param pays Le pays à enregistrer
+     */
     public void setPays(String pays) {
         information.put("pays", pays);
     }
 
-    /** Définit le genre de l'étudiant. */
+    /**
+     * Définit le genre de l'étudiant.
+     * @param gender Le genre ("true" pour masculin, "false" pour féminin)
+     */
     public void setGender(String gender) {
         information.put("gender", gender);
     }
 
-    /** Définit le genre du partenaire. */
+    /**
+     * Définit le genre souhaité pour le partenaire.
+     * @param pairGender Le genre ("true" pour masculin, "false" pour féminin)
+     */
     public void setPairGender(String pairGender) {
         information.put("pairGender", pairGender);
     }
 
-    /** Définit si l'invité est allergique aux animaux ("true" ou "false"). */
+    /**
+     * Définit si l'invité est allergique aux animaux.
+     * @param guestAnimalAllergy "true" si allergique, "false" sinon
+     */
     public void setGuestAnimalAllergy(String guestAnimalAllergy) {
         information.put("guestAnimalAllergy", guestAnimalAllergy);
     }
 
-    /** Définit si l'hôte possède des animaux ("true" ou "false"). */
+    /**
+     * Définit si l'hôte possède des animaux.
+     * @param hostHasAnimal "true" si l'hôte a des animaux, "false" sinon
+     */
     public void setHostHasAnimal(String hostHasAnimal) {
         information.put("hostHasAnimal", hostHasAnimal);
     }
 
-    /** Définit les préférences alimentaires de l'invité. */
+    /**
+     * Définit les préférences alimentaires de l'invité.
+     * @param guestFood Les préférences séparées par des virgules
+     */
     public void setGuestFood(String guestFood) {
         information.put("guestFood", guestFood);
     }
 
-    /** Définit les préférences alimentaires de l'hôte. */
+    /**
+     * Définit les préférences alimentaires de l'hôte.
+     * @param hostFood Les préférences séparées par des virgules
+     */
     public void setHostFood(String hostFood) {
         information.put("hostFood", hostFood);
     }
 
-    /** Définit l'historique de l'étudiant. */
+    /**
+     * Définit l'historique de l'étudiant.
+     * @param history L'historique à enregistrer
+     */
     public void setHistory(String history) {
         information.put("history", history);
     }
 
-    /** Définit les hobbies de l'étudiant. */
+    /**
+     * Définit les hobbies de l'étudiant.
+     * @param hobbies Les centres d'intérêt séparés par des virgules
+     */
     public void setHobbies(String hobbies){
         information.put("hobbies", hobbies);
     }
 
-    /** Retourne le prénom de l'étudiant. */
+    /**
+     * Retourne le prénom de l'étudiant.
+     * @return Le prénom de l'étudiant
+     */
     public String getPrenom() {
         return information.get("prenom");
     }
 
-    /** Retourne le nom de l'étudiant. */
+    /**
+     * Retourne le nom de l'étudiant.
+     * @return Le nom de l'étudiant
+     */
     public String getNom() {
         return information.get("nom");
     }
 
-    /** Retourne la date de naissance sous forme d'objet LocalDate. */
+    /**
+     * Retourne la date de naissance sous forme d'objet LocalDate.
+     * @return La date de naissance
+     */
     public LocalDate getDateNaissance() {
         return LocalDate.parse(information.get("dateNaissance"));
     }
+
+    /**
+     * Formate une date LocalDate en chaîne de caractères.
+     * @param dateNaissance La date à formater
+     * @return La date formatée en "yyyy-MM-dd"
+     */
     public String getDateNaissance(LocalDate dateNaissance){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return dateNaissance.format(formatter);
     }
 
-    /** Retourne le pays d'origine de l'étudiant. */
+    /**
+     * Retourne le pays d'origine de l'étudiant.
+     * @return Le pays de l'étudiant
+     */
     public String getPays() {
         return information.get("pays");
     }
 
-    /** Retourne le genre de l'étudiant. */
+    /**
+     * Retourne le genre de l'étudiant.
+     * @return "true" pour masculin, "false" pour féminin
+     */
     public String getGender() {
         return information.get("gender");
     }
 
-    /** Retourne le genre du partenaire. */
+    /**
+     * Retourne le genre souhaité pour le partenaire.
+     * @return "true" pour masculin, "false" pour féminin
+     */
     public String getPairGender() {
         return information.get("pairGender");
     }
 
-    /** Indique si l'invité est allergique aux animaux. */
+    /**
+     * Indique si l'invité est allergique aux animaux.
+     * @return true si allergique, false sinon
+     */
     public boolean getGuestAnimalAllergy() {
         return information.get("guestAnimalAllergy").equals("true");
     }
 
-    /** Indique si l'hôte possède des animaux. */
+    /**
+     * Indique si l'hôte possède des animaux.
+     * @return true si l'hôte a des animaux, false sinon
+     */
     public boolean getHostHasAnimal() {
         return information.get("hostHasAnimal").equals("true");
     }
 
-    /** Retourne les préférences alimentaires de l'invité. */
+    /**
+     * Retourne les préférences alimentaires de l'invité sous forme de liste.
+     * @return ArrayList des préférences alimentaires
+     */
     public ArrayList<String> getGuestFood(){
         ArrayList<String> gestFood = new ArrayList<String>();
-        String[] food = information.get("gestFood").split(",");
+        String[] food = information.get("guestFood").split(",");
 
         for (String valeur : food){
             gestFood.add(valeur);
         }
-    return gestFood;
+        return gestFood;
     }
 
-    /** Retourne les préférences alimentaires de l'hôte. */
+    /**
+     * Retourne les préférences alimentaires de l'hôte sous forme de liste.
+     * @return ArrayList des préférences alimentaires
+     */
     public ArrayList<String> getHostFood(){
         ArrayList<String> hostFood = new ArrayList<String>();
         String[] food = information.get("hostFood").split(",");
@@ -261,58 +360,67 @@ public class Student {
         for (String valeur : food){
             hostFood.add(valeur);
         }
-    return hostFood;
+        return hostFood;
     }
 
-    /** Retourne l'historique de l'étudiant. */
+    /**
+     * Retourne l'historique de l'étudiant.
+     * @return L'historique de l'étudiant
+     */
     public String getHistory() {
         return information.get("history");
     }
 
-    /**retourn la liste de tout les hobbies de l'etudiant */
+    /**
+     * Retourne la liste des hobbies de l'étudiant.
+     * @return ArrayList des centres d'intérêt
+     */
     public ArrayList<String> getHobbies() {
-
-    ArrayList<String> hobbies = new ArrayList<>();
-    String[] hobbie = information.get("hobbies").split(",");
+        ArrayList<String> hobbies = new ArrayList<>();
+        String[] hobbie = information.get("hobbies").split(",");
 
         for (String valeur : hobbie){
             hobbies.add(valeur);
         }
-    return hobbies;
+        return hobbies;
     }
 
+    /**
+     * Détermine si l'étudiant est un hôte (vient d'Italie).
+     * @return true si l'étudiant est un hôte, false sinon
+     */
     public boolean isHote(){
-        if (this.getPays().equals("Italie")){
-            return true;
-        }
-        return false;
+        return this.getPays().equals("Italie");
     }
-    // calculer l'affinité entre deux étudiants (lui avec un autre.)
+
+    /**
+     * Calcule l'affinité entre cet étudiant et un autre étudiant.
+     * L'affinité est basée sur:
+     * - Compatibilité des genres (1.5 points par correspondance)
+     * - Hobbies communs (1 point pour 1 hobby, 2 points pour 2+ hobbies)
+     * - Proximité des dates de naissance (1 point si <1.5 ans d'écart)
+     * 
+     * @param student L'autre étudiant avec lequel calculer l'affinité
+     * @return Le score d'affinité total
+     */
     public double calculAffinite(Student student) {
         double affinite = 0;
-
-        // Calcul de l'affinité basée sur les genres : 
-        // si le genre partenaire correspond au genre de l'autre personne, on ajoute 1.5
 
         if (this.getPairGender() == student.getGender()) {
             affinite = affinite + 1.5;
         }
-        // pareille dans l'autre sens
         if (this.getGender() == student.getPairGender()) {
             affinite = affinite + 1.5;
         }
 
-        // Calcul du nombre de hobbies communs
         int nbCommonHobbies = 0;
         for (int i = 0; i < this.getHobbies().size(); i = i + 1) {
             for (int j = 0; j < student.getHobbies().size(); j = j + 1) {
-                // Si un hobby est commun, on ajoute un au compteur
                 if (this.getHobbies().get(i).equals(student.getHobbies().get(j))) {
                     nbCommonHobbies = nbCommonHobbies + 1;
                 }
             }
         }
-        // Selon le nombre de hobbies communs, on augmente l'affinité
         if (nbCommonHobbies == 1) {
             affinite = affinite + 1;
         }
@@ -320,20 +428,21 @@ public class Student {
             affinite = affinite + 2;
         }
 
-        // Calcul de la proximité des dates de naissance (en jours)
         int jourlimit = (int) (365 * 1.5);
         int thisDate = this.getDateNaissance().getYear() * 365 + this.getDateNaissance().getDayOfYear();
         int stdDate = student.getDateNaissance().getYear() * 365 + student.getDateNaissance().getDayOfYear();
 
-        // Si la différence est inférieure à 1.5 an, on ajoute 1 à l'affinité
         if (Math.abs(thisDate - stdDate) < jourlimit) {
             affinite++;
         }
 
-        // Retourne le score total d'affinité calculé
         return affinite;
     }
     
+    /**
+     * Retourne une représentation textuelle de l'étudiant.
+     * @return Le nom et prénom de l'étudiant séparés par un espace
+     */
     @Override
     public String toString(){
         return ""+getNom()+" "+getPrenom();
